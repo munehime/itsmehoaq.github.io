2a1f9c801ad4d73905d7d44b6833dcee3dd1b69d
itsmehoaq <hoaqvvu06@gmail.com>
2023-09-21 22:23:26 +0700

    update home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,8 @@
 import { Link, useLocation } from "react-router-dom";
-import twitter from "../dep/img/twitter.png"
-import twitch from "../dep/img/twitch.png"
-import osu from "../dep/img/osu.png"
+
 import "./style/Navbar.css"
 function Home() {
-    return (useLocation().pathname === "/" ? 
+    return (useLocation().pathname === "/" ?
         <Link to="/" className="NavbarButton" onClick={ (event) =>event.preventDefault()}>Home</Link> :
         <Link to="/" className="NavbarButton">Home</Link>
     );
@@ -20,22 +18,11 @@ function Navbar() {
         <div className="Navbar">
             <div className="NavbarContainer">
                 <div className="NavbarBadge">
-                    206(Skin)DB
+                    Hoaq Skins Database
                 </div>
                 <Home />
                 <Skins />
             </div>
-            {/*<div className="NavbarContainer">*/}
-            {/*    <a href="https://twitter.com/itsmehoaq">*/}
-            {/*        <img src={twitter} className="NavbarIcon" alt="Twitter"/>*/}
-            {/*    </a>*/}
-            {/*    <a href="https://www.twitch.tv/itsmehoaq">*/}
-            {/*        <img src={twitch} className="NavbarIcon" alt="Twitch" />*/}
-            {/*    </a>*/}
-            {/*    <a href="https://osu.ppy.sh/users/7696512">*/}
-            {/*        <img src={osu} className="NavbarIcon" alt="osu" />*/}
-            {/*    </a>*/}
-            {/*</div>*/}
         </div>
     )
 }
